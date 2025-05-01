7d8aa1f94fe3331d921428694376d5e532e465b3
Chris Watson <christopher.watson@elginwhite.com>
2025-05-01 13:20:41 +0100

    Added package for the server. Re-organised files inside of the new server package.

---
+++ packages/server/src/handlers/da/SailFDC3Server.ts
@@ -0,0 +1,51 @@
+import { DesktopAgentHelloArgs, TabDetail } from "@finos/fdc3-sail-common"
+import {
+  ChannelState,
+  ChannelType,
+  DefaultFDC3Server,
+} from "@finos/fdc3-web-impl"
+import { SailServerContext } from "./SailServerContext"
+
+export const mapChannels = (channels: TabDetail[]): ChannelState[] =>
+  channels.map(({ id, icon, background }) => ({
+    id,
+    type: ChannelType.user,
+    displayMetadata: {
+      name: id,
+      glyph: icon,
+      color: background,
+    },
+    context: [],
+  }))
+
+/**
+ * Extends BasicFDC3Server to allow for more detailed (and changeable) user channel metadata
+ * as well as user-configurable SailDirectory.
+ */
+export class SailFDC3Server extends DefaultFDC3Server {
+  readonly serverContext: SailServerContext
+
+  constructor(
+    sailServerContext: SailServerContext,
+    helloArgs: DesktopAgentHelloArgs,
+  ) {
+    super(
+      sailServerContext,
+      sailServerContext.directory,
+      mapChannels(helloArgs.channels),
+      true,
+      60000,
+      20000,
+    )
+    sailServerContext.directory.replace(helloArgs.directories)
+    this.serverContext = sailServerContext
+  }
+
+  getDirectory() {
+    return this.serverContext.directory
+  }
+
+  getServerContext() {
+    return this.serverContext
+  }
+}
